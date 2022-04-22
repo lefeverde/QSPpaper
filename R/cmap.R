d688dfcbfd5d787c37b888b9ff3e4450f7e0600b
Daniel Lefever <lefeverde@gmail.com>
2022-04-22 15:20:12 -0400

    Updated to reflect latest version of paper

--- R/cmap.R
+++ R/cmap.R
@@ -411,4 +411,187 @@ add_p_vals_to_cmap_para <- function(cmap_res, perm_res){
 }
 
 
+#' Normalizes the connectivity score by cell type
+#'
+#' Scores are normalized by cell type.
+#'
+#'
+#' @param cs_mat data.frame of CS scores
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calculate_ncs <- function(cs_mat){
+  # There's not enough ncs sigs for all the different cell types so
+  # I'm not just not going to bother with it for the time being.
 
+
+  mu_pos_df <- cs_mat %>%
+    filter(cmap_score > 0) %>%
+    # filter(is_ncs_sig == 1) %>%
+    # filter(is_touchstone == 1) %>%
+    group_by(sig_idx, cell_iname) %>%
+    summarise(mu_pos=mean(cmap_score)) %>%
+    ungroup
+
+
+  mu_neg_df <- cs_mat %>%
+    filter(cmap_score < 0) %>%
+    # filter(is_ncs_sig == 1) %>%
+    # filter(is_touchstone == 1) %>%
+    group_by(sig_idx, cell_iname) %>%
+    summarise(mu_neg=mean(cmap_score)) %>%
+    ungroup
+
+  out_df <- left_join(cs_mat, mu_pos_df) %>% left_join(., mu_neg_df)
+  out_df <- out_df %>%
+    mutate(norm_cmap_score=if_else(cmap_score > 0, cmap_score/mu_pos, -cmap_score/mu_neg)) %>%
+    select(-mu_neg, -mu_pos)
+  # This is done to set the divide by 0 values to 0
+  out_df$norm_cmap_score[is.na(out_df$norm_cmap_score)] <- 0
+  return(out_df)
+}
+
+#' Calculates pert score per cell type
+#'
+#' @inheritParams calculate_ncs
+#' @param quant_thresh percentile threshold. Default is .67 which was used in Subramanian 2017
+#' @param quant_type This is how the quantiles are calculated. See \link[stats]{quantile}
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calculate_pert_cell_score <- function(cs_mat, quant_thresh=.67, quant_type=5){
+  hi_thresh <- quant_thresh
+  lo_thresh <- 1 - quant_thresh
+  # Doign a group_by with 3 vars seems to slow the code way down
+  # splitting is faster
+  out_df <- split(cs_mat, cs_mat$sig_idx) %>%
+    map_dfr(function(x){
+      sig_idx <- unique(x$sig_idx)
+      pert_cell_mat <- x %>%
+        group_by(pert_id, cell_iname) %>%
+        summarise(pert_cell_hi=quantile(norm_cmap_score, probs=hi_thresh, type=quant_type),
+                  pert_cell_lo=quantile(norm_cmap_score, probs=lo_thresh, type=quant_type),
+                  n_sigs=n()) %>%
+        ungroup %>%
+        mutate(pert_cell_sum_score=if_else(abs(pert_cell_hi) > abs(pert_cell_lo), pert_cell_hi, pert_cell_lo))
+      pert_cell_mat <- cbind(sig_idx, pert_cell_mat)
+    })
+
+  return(out_df)
+
+}
+
+
+#' Calculates the perturbagen summary score
+#'
+#' @param pert_cell_mat output from \link{calculate_pert_cell_score}
+#' @inheritParams calculate_pert_cell_score
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calculate_pert_score <- function(pert_cell_mat, quant_thresh=.67, quant_type=5){
+  hi_thresh <- quant_thresh
+  lo_thresh <- 1 - quant_thresh
+  # Doign a group_by with 3 vars seems to slow the code way down
+  # splitting is faster
+  out_df <- split(pert_cell_mat, pert_cell_mat$sig_idx) %>%
+    map_dfr(function(x){
+      sig_idx <- unique(x$sig_idx)
+      pert_mat <- x %>%
+        group_by(pert_id) %>%
+        summarise(pert_hi=quantile(pert_cell_sum_score, probs=hi_thresh, type=quant_type),
+                  pert_lo=quantile(pert_cell_sum_score, probs=lo_thresh, type=quant_type),
+                  n_sigs=n()) %>%
+        ungroup %>%
+        mutate(pert_sum_score=if_else(abs(pert_hi) > abs(pert_lo), pert_hi, pert_lo))
+      pert_mat <- cbind(sig_idx, pert_mat)
+    })
+  out_df <- out_df %>%
+    group_by(sig_idx) %>%
+    arrange(pert_sum_score, .by_group = TRUE) %>%
+    mutate(drug_idx=row_number()) %>%
+    ungroup
+
+  return(out_df)
+
+}
+
+
+#' Returns the top 20 compounds using the best score approach
+#'
+#' @param best_cs_mat output from \link{best_score_mat}
+#'
+#' @return
+#' @export
+#'
+#' @examples
+best_score_top20 <- function(best_cs_mat){
+  top20_mat <-  best_cs_mat %>%
+    distinct %>%
+    group_by(sig_idx) %>%
+    arrange(cmap_score, .by_group=TRUE) %>%
+    slice(1:20) %>%
+    ungroup
+  top20_mat <- top20_mat %>%
+    group_by(pert_iname) %>%
+    summarise(freq=n(), unique_inst=length(unique(sig_id))) %>%
+    arrange(desc(freq))
+
+  return(top20_mat)
+}
+
+#' Chooses the best score per compound
+#'
+#' @inheritParams calculate_ncs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+best_score_mat <- function(cs_mat){
+  best_cs_mat <- cs_mat %>%
+    filter(cmap_score < 0) %>%
+    filter(fdr_p_value < .05) %>%
+    group_by(sig_idx, pert_iname) %>%
+    arrange(cmap_score, .by_group=TRUE) %>%
+    slice(1) %>%
+    ungroup %>%
+    group_by(sig_idx) %>%
+    arrange(cmap_score, .by_group=TRUE) %>%
+    mutate(drug_idx=row_number()) %>%
+    ungroup
+}
+
+#' Returns the top 20 compounds after performing maximum quantile statistic
+#'
+#' @param pert_mat
+#'
+#' @return
+#' @export
+#'
+#' @examples
+lincs2017_score_top20 <- function(pert_mat){
+  clean_mat <- pert_mat %>%
+    group_by(sig_idx, pert_iname) %>%
+    arrange(pert_sum_score, .by_group = TRUE) %>%
+    slice(1) %>%
+    ungroup
+
+  top20_mat <-  clean_mat %>%
+    distinct %>%
+    group_by(sig_idx) %>%
+    arrange(pert_sum_score, .by_group=TRUE) %>%
+    slice(1:20, with_ties=FALSE) %>%
+    ungroup %>%
+    group_by(pert_iname) %>%
+    summarise(freq=n()) %>%
+    arrange(desc(freq))
+  return(top20_mat)
+
+}
